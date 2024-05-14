03fd8094cf7cbfc0fce19903217e0d545578a1c3
PetrikDiak <diak@petrik.hu>
2024-05-14 11:33:20 +0200

    post függvény errort nem ad vissza ellenben nem vesz fel az sqlbe semmilyen új elemet

--- backend/index.js
+++ backend/index.js
@@ -14,18 +14,18 @@ const bl_gyozelmek = mysql.createPool({
 }).promise();
 
 app.get('/csapatok', async (req, res) => {
-    const [rows, fields] = await bl_gyozelmek.query('SELECT id, klubnev, gyozelmek_szama, legutobb_gyozott, mvp FROM bl_adatok');
+    const [rows, fields] = await bl_gyozelmek.query('SELECT id, klubnev, gyozelmek_szama, CAST(legutobb_gyozott AS char) legutobb_gyozott, mvp FROM bl_adatok');
     res.send(rows);
 }); 
 app.post('/csapatok', async (req, res) => {
     console.log(req.body);
-    const [data,fields] = await bl_gyozelmek.query('SELECT id, klubnev, gyozelmek_szama, legutobb_gyozott, mvp FROM bl_adatok');
-    res.status.send(201).send({
-            id: data.insertId,
+    const [data,fields] = await bl_gyozelmek.query('SELECT id, klubnev, gyozelmek_szama, CAST(legutobb_gyozott AS char) legutobb_gyozott, mvp FROM bl_adatok');
+    res.status(201).send({
             klubnev: req.body.klubnev,
             gyozelmek_szama: req.body.gyozelmek_szama,
             legutobb_gyozott: req.body.legutobb_gyozott,
-            mvp: req.body.mvp
+            mvp: req.body.mvp,
+            id: data.insertId
     });
 });
 
