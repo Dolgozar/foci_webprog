7a4ad36c0c9147315e15e037743cb07e56dc9351
PetrikDiak <diak@petrik.hu>
2024-05-14 10:33:07 +0200

    post hozzáadása a backendhez

--- backend/index.js
+++ backend/index.js
@@ -17,5 +17,15 @@ app.get('/csapatok', async (req, res) => {
     const [rows, fields] = await rendelesek.query('SELECT id, klubnev, gyozelmek_szama, legutobb_gyozott, mvp FROM foci-webprog');
     res.send(rows);
 }); 
+app.post('/csapatok', async (req, res) => {
+    const [data,fields] = await bl_gyozelmek.query('SELECT id, klubnev, gyozelmek_szama, legutobb_gyozott, mvp FROM bl-adatok' )
+    res.status.send(201).send({
+            id: data.insertId,
+            klubnev: req.body.klubnev,
+            gyozelmek_szama: req.body.gyozelmek_szama,
+            legutobb_gyozott: req.body.legutobb_gyozott,
+            mvp: req.body.mvp
+    });
+})
 
 app.listen(3000);
