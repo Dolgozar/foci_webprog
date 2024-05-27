b942412451b076df851caccf7d409e42471835fb
Dávid Szabó <szabo.david@petrik.hu>
2024-05-27 21:51:33 +0200

    POST mukodik

--- backend/index.js
+++ backend/index.js
@@ -17,17 +17,18 @@ app.get('/csapatok', async (req, res) => {
     const [rows, fields] = await bl_gyozelmek.query('SELECT id, klubnev, gyozelmek_szama, CAST(legutobb_gyozott AS char) legutobb_gyozott, mvp FROM bl_adatok');
     res.send(rows);
 });
- 
+
 app.post('/csapatok', async (req, res) => {
     console.log(req.body);
-    const [data,fields] = await bl_gyozelmek.query('SELECT id, klubnev, gyozelmek_szama, CAST(legutobb_gyozott AS char) legutobb_gyozott, mvp FROM bl_adatok');
+    const [data, fields] = await bl_gyozelmek.query('INSERT INTO bl_adatok (klubnev, gyozelmek_szama, legutobb_gyozott, mvp) VALUES(?,?,?,?)', [req.body.klubnev, req.body.gyozelmek_szama, req.body.legutobb_gyozott, req.body.mvp]);
     res.status(201).send({
+            id: data.insertId,
             klubnev: req.body.klubnev,
             gyozelmek_szama: req.body.gyozelmek_szama,
             legutobb_gyozott: req.body.legutobb_gyozott,
-            mvp: req.body.mvp,
-            id: data.insertId
+            mvp: req.body.mvp
     });
+    res.send("Sikeres Felvétel!")
 });
 
 app.listen(3000);
