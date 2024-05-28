77307aac719a1566ced293b397b8fce3e513e9fe
John Doe <johndoe@email.com>
2024-05-28 11:50:33 +0200

    Design background, bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,83 +3,97 @@ import './App.css'
 //import { Focits } from './Focits.ts'
 import { Foci } from '../components/Foci'
 import { Focits } from './Focits'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
-  const [data, setData] = useState([] as Focits[])
+  const [data, setData] = useState([] as Focits[]);
   const klubref = useRef<HTMLInputElement>(null);
-  const gyozelemref =  useRef<HTMLInputElement>(null);
+  const gyozelemref = useRef<HTMLInputElement>(null);
   const legutobbref = useRef<HTMLInputElement>(null);
   const mvpref = useRef<HTMLInputElement>(null);
 
-
   useEffect(() => {
-    async function load(){
-      const response = await fetch('http://localhost:3000/csapatok')
+    async function load() {
+      const response = await fetch('http://localhost:3000/csapatok');
       const data = await response.json() as Focits[];
-      setData(data)
+      setData(data);
     }
     load();
-  }, [data])
+  }, [data]);
 
-  async function addKlub(event: FormEvent){
+  async function addKlub(event: FormEvent) {
     event.preventDefault();
     const klub = klubref.current?.value;
     const gyozelemdb = gyozelemref.current?.value;
     const legutobb = legutobbref.current?.value;
     const mvp = mvpref.current?.value;
 
-
     const csapat = {
       klubnev: klub,
-      gyozelmek_szama: gyozelemdb, 
+      gyozelmek_szama: gyozelemdb,
       legutobb_gyozott: legutobb,
       mvp: mvp,
-    }
+    };
     const res = await fetch('http://localhost:3000/csapatok', {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify(csapat),
-      headers: {"Content-Type": "application/json"}
+      headers: { 'Content-Type': 'application/json' },
     });
-    if(res.ok){
-      console.log("Hozzáadva!")
-      klubref.current!.value = "";
-      gyozelemref.current!.value = "";
-      legutobbref.current!.value = "";
-      mvpref.current!.value = "";
+    if (res.ok) {
+      console.log('Hozzáadva!');
+      klubref.current!.value = '';
+      gyozelemref.current!.value = '';
+      legutobbref.current!.value = '';
+      mvpref.current!.value = '';
     }
   }
 
-  return <div>
-  <form className="felvetel" onSubmit={event => addKlub(event)}>
-  <h1 className='title'>Csapat Hozzáadása</h1>
-  <input type="text" ref={klubref} placeholder='Klubnév...'/><br />
-  <input type="number" min="0" ref={gyozelemref} placeholder='Győzelmek...'/><br />
-  <input type="date" ref={legutobbref} placeholder='Legutóbb...'/><br />
-  <input type="text" ref={mvpref} placeholder='Mvp...'/><br />
-  <button type='submit'>Felvétel</button>
-  <br/>
-  <br/>
-</form>
+  return (
+    <div className="container">
+      <form className="felvetel" onSubmit={(event) => addKlub(event)}>
+        <h1 className="title">Csapat Hozzáadása</h1>
+        <div className="mb-3">
+          <input type="text" className="form-control" ref={klubref} placeholder="Klubnév..." />
+        </div>
+        <div className="mb-3">
+          <input type="number" min="0" className="form-control" ref={gyozelemref} placeholder="Győzelmek..." />
+        </div>
+        <div className="mb-3">
+          <input type="date" className="form-control" ref={legutobbref} placeholder="Legutóbb..." />
+        </div>
+        <div className="mb-3">
+          <input type="text" className="form-control" ref={mvpref} placeholder="Mvp..." />
+        </div>
+        <button type="submit" className="btn btn-primary">Felvétel</button>
+      </form>
 
-
-<table className='tabla'>
-  <thead>
-    <tr>
-      <th>Klubnév</th>
-      <th>Győzelmek száma</th>
-      <th>Legutóbb győzőtt</th>
-      <th>Mvp</th>
-    </tr>
-  </thead>
-  <tbody>
-    {
-        data.map((data) => <Foci key={data.id} klubnev={data.klubnev} gyozelem_db={data.gyozelmek_szama} legutobb_w={data.legutobb_gyozott} mvp={data.mvp}></Foci>) 
-    }
-  </tbody>
-</table>
-</div>
+      <table className="table mt-5">
+        <thead>
+          <tr>
+            <th>Klubnév</th>
+            <th>Győzelmek száma</th>
+            <th>Legutóbb győzőtt</th>
+            <th>Mvp</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((data) => (
+            <Foci
+              key={data.id}
+              klubnev={data.klubnev}
+              gyozelem_db={data.gyozelmek_szama}
+              legutobb_w={data.legutobb_gyozott}
+              mvp={data.mvp}
+            />
+          ))}
+        </tbody>
+      </table>
+      <footer className="green-footer">
+        Készítette: Dohy Olivér, Gál Dániel, Szabó Dávid
+      </footer>
+    </div>
+  );
 }
 
-
-
-export default App
+export default App;
